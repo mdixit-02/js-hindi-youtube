30d9e5a31dd814f50db98fe07ad5a87d132f08e2
Megha Dixit <116000785+mdixit-02@users.noreply.github.com>
2024-10-01 04:13:42 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as new version
+
+// alert(3 + 3) we are using node.js not browser
+
+//console.log(3 + 3)
+
+//console.log("Megha")    //code readability should be high
+
+let name = "Megha"
+let age = 24
+let isLoggedIn = true;
+
+let state; //undefined
+let state1 = "null" //value will become as 0.
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+
+//symbol => for unique config
+//object
+
+console.log(typeof age);
+console.log(typeof "Megha")
+console.log(typeof null)    //null is not a type, it is a Object in JS.
+console.log(typeof undefined)     //undefined is a Datatype in Javascript
