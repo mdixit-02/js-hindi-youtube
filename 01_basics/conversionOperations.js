45da3bd610c2e3ce6b6621fe78151a624736b9ab
Megha Dixit <116000785+mdixit-02@users.noreply.github.com>
2024-10-03 02:35:52 +0000

    conersion of datatypes

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,31 @@
+let score = null;
+
+//console.log(score);
+
+// both way we can define
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueOfNumber = Number(score);
+//console.log(typeof valueOfNumber);
+//console.log(valueOfNumber);
+
+//33 => 33, type => number
+//33abc => Nan(Not a number), type => number
+//true => 1 ; false => 0
+//null => number, type => 0
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true
+//0 => false
+//"" => false
+//"megha" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber)
+console.log(typeof stringNumber)
