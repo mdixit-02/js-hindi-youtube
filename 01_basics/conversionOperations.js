db38ec1dc296a482fde83c4b08262a2a8215af71
Megha Dixit <116000785+mdixit-02@users.noreply.github.com>
2024-10-04 02:24:05 +0000

    Operations

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -17,7 +17,7 @@ let valueOfNumber = Number(score);
 
 let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 //1 => true
 //0 => false
@@ -27,5 +27,44 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+//********************* Operations **************************/
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)  //gives power value, 2 power 3
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "Hello"
+let str2 = " Megha"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log(1 + "2")  //12
+// console.log( "1" + 2) //12
+// console.log( "1" + 2 + 2) //122 (If first number is string then it will act as String only)
+// console.log(1 + 2 + "2") //32 (If first numbers are not string, then it will add those first and then attach the string value)
+// console.log(1 + "2" + "2") // 122
+// console.log(1 + 2 + "2" + 3) //323
+// console.log( 3 + 4 * 5 % 3) //5
+// console.log( (3 + 4) * 5 % 3) //2
+
+// console.log( 5 % 3) // 2 (remainder)
+
+console.log(true) // true
+console.log(+true) // 1
+//console.log(true+) // It will give error as + comes after true
+console.log(+"") //0
+
+let gameCounter = 100
+gameCounter++; //101
+//++gameCounter; //101
+console.log(gameCounter)
